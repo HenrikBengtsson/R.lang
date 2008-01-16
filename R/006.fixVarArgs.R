65e141f006bb7b22a612bcd4855337b4fef013f9
Henrik Bengtsson <hb@aroma-project.org>
2008-01-16 03:20:14 +0000

    Version: 0.21 [2008-01-15] o BUG FIX: The Windows binaries for R.lang v0.20 that was built   on R v2.5.0 gave "Error in nchar.default(name) :   2 arguments passed to .Internal(nchar) which requires 3   Error in library(R.lang) : .First.lib failed for 'R.lang'"   when loaded on R v2.6.0.  In order to keep backward   compatibility to R v2.0.0 (does any need that?), I did   a workaround that I think works. o Package pass R CMD check on R v2.6.1 and R v2.7.0 devel.   No tests and no promises are done for version before that.
    git-svn-id: https://biostat-svn.berkeley.edu/svn/trunk/Rpacks/R.lang/R.lang@3842 40a15fb2-e712-0410-b876-b1a1dc003736

--- R/006.fixVarArgs.R
+++ R/006.fixVarArgs.R
@@ -3,11 +3,19 @@
 
 substring <- appendVarArgs(substring)
 replace <- appendVarArgs(replace)
-nchar <- appendVarArgs(nchar)
+
+# The base::nchar() of R v2.5.0 and R v2.6.0 are not compatible.
+# The below is a workaround for this.
+# nchar <- appendVarArgs(nchar)
+nchar <- function(...) {
+  base::nchar(...);
+}
 
 
 ############################################################################
 # HISTORY:
+# 2008-01-15
+# o Added workaround for appendVarArgs(nchar).
 # 2005-02-20
 # o Created to please R CMD check.
 ############################################################################
